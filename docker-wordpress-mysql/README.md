da68e650a88e548505b33a824d4b59a7f5a9ad7a
dino <dino@mitre.org>
2019-03-28 23:23:37 -0400

    docker-wordpress-mysql

--- docker-wordpress-mysql/README.md
+++ docker-wordpress-mysql/README.md
@@ -1,5 +1,4 @@
-# Deployment of WordPress+MySQL Containers with Docker Compose
-
+# Deployment of WordPress + MySQL Containers with Docker Compose
 
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2Fdocker-wordpress-mysql%2Fazuredeploy.json" target="_blank">
 	<img src="http://azuredeploy.net/deploybutton.png"/>
@@ -8,12 +7,13 @@
     <img src="http://armviz.io/visualizebutton.png"/>
 </a>
 
-This template allows you to deploy an Ubuntu Server 16.04.0-LTS VM with Docker (using the [Docker Extension][ext])
-and starts a WordPress container listening an port 80 which uses MySQL database running
-in a separate but linked Docker container, which are created using [Docker Compose][compose]
-capabilities of the [Azure Docker Extension][ext].
+This template allows you to deploy an Ubuntu Server 16.04.0-LTS VM with Docker (without using the 
+[Docker Extension][ext], but with a custom dino script instead) and start a WordPress container 
+listening an port 8000 which uses MySQL database running in a separate but linked Docker container, 
+which are created using [Docker Compose].
 
-This template is configured for AzureUSGovernment, and specifically for MI TCO490, Spring 2019. Will not work outside of that subscription, sorry.
+This template is configured for AzureUSGovernment, and specifically for MI TCO490, Spring 2019. Will 
+probably work work outside of that subscription, but if it doesn't I don't know who wrote this :-)
 
 Command line (remove CRs):
 
@@ -23,5 +23,14 @@ https://raw.githubusercontent.com/dinorows/TCO490/master/docker-wordpress-mysql/
 'My5q1P@s5w0rd!'},'adminUsername': {'value': 'dino'},'adminPassword': {'value':'passw0rd12345!'},
 'dnsNameForPublicIP': {'value': 'dinopublicip617'}}"
 
-[ext]: https://github.com/Azure/azure-docker-extension
-[compose]: https://docs.docker.com/compose
+Note that the azure deploy template contains a custom script extension version 2 for Linux machines
+that installs docker and docker-compose, as well as references a custom docker-compose .yml script 
+to install and hook up a wordpress and a mysql container! See here: 
+https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux
+
+After deployment and following container install and initialization, WordPress should be running on 
+port 8000 of your Docker Host, and you can complete the “famous five-minute installation” as a 
+WordPress administrator.
+
+To access first time and configure: #http://<vm-publicip>:8000 
+To administer thereafter: #http://<vm-publicip>:8000/wp-admin/
---
+++ docker-wordpress-mysql/scripts/install-docker-wordpress-mysql.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# just in case you wand to install dotnet core..
+#sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+#sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+#sudo apt-get update
+#sudo apt-get install -y dotnet-dev-1.0.0-preview2-003121
+
+# ..and download your web app
+#sudo wget https://raw.githubusercontent.com/dinorows/TCO490/master/docker-wordpress-mysql/README.md/myapp/myapp-pub.tar /
+#sudo mkdir /opt/myapp
+#sudo tar -xf myapp-pub.tar -C /opt/myapp
+
+#
+# All we're going to do here is download pre-fab containers and hook them up with docker-compose
+#
+
+# Create an empty project directory and download the .yml docker-compose script from my github
+mkdir my_wordpress && cd my_wordpress
+#sudo apt-get install lynx
+#lynx -dump https://raw.githubusercontent.com/dinorows/TCO490/master/docker-wordpress-mysql/docker-compose.yml > docker-compose.yml
+sudo wget https://raw.githubusercontent.com/dinorows/TCO490/master/docker-wordpress-mysql/docker-compose.yml -O .
+
+# update
+sudo apt-get update
+sudo apt-get install curl
+
+# install docker
+#sudo curl -fsSL https://get.docker.com/ | sh
+sudo apt-get install docker.io
+
+# You can check that the installation was successful with the following test program:
+#sudo docker run hello-world
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl restart docker
+
+# docker-compose comes with command completion for the bash and zsh shell.
+# Install command completion
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+# run docker-compose to download and then hook up wordpress and mysql containers, 
+# as specified in script docker-compose.yml
+sudo docker-compose up -d
+
+# to check on container ops:
+#docker ps -all
+
+# At this point, WordPress should be running on port 8000 of your Docker Host, and you can 
+# complete the “famous five-minute installation” as a WordPress administrator.
+
+# Note: The WordPress site is not immediately available on port 8000 because the containers are 
+# still being initialized and may take a couple of minutes before the first load.
+
+# If you are using Docker Machine, you can run the command docker-machine ip MACHINE_VM to get 
+# the machine address, and then open http://MACHINE_VM_IP:8000 in a web browser.
+
+# wordpress:
+#http://<vm-publicip>:8000 to access first time and configure
+#http://<vm-publicip>:8000/wp-admin/ to administer
