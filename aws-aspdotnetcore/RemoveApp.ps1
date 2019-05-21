2b6db02195fc6601fdd5caa47bda62d039162091
dino konstantopoulos <dinorows@users.noreply.github.com>
2019-05-20 23:31:37 -0400

    Update RemoveApp.ps1

--- aws-aspdotnetcore/RemoveApp.ps1
+++ aws-aspdotnetcore/RemoveApp.ps1
@@ -1,1 +1,1 @@
-﻿C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value C:\inetpub\wwwroot}
+﻿C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value C:\inetpub\wwwroot}
