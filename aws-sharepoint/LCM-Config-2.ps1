c87ff849199616c1f5b900eed8a18010177a3a7d
dino konstantopoulos <dinorows@users.noreply.github.com>
2019-06-12 19:14:19 -0400

    Delete LCM-Config-2.ps1

--- aws-sharepoint/LCM-Config-2.ps1
+++
@@ -1,5 +0,0 @@
-# This block sets the LCM configuration to what we need for QS
-[DSCLocalConfigurationManager()]
-    
-# Sets LCM Configuration to MOF generated in previous command
-Set-DscLocalConfigurationManager -Path 'C:\AWSQuickstart\LCMConfig' 
