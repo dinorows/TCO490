3c7e1d991c6b2d060422193cf80c312992bab10d
dino konstantopoulos <dinorows@users.noreply.github.com>
2019-06-22 18:14:33 -0400

    Update LCM-Config-2.ps1

--- aws-sharepoint/LCM-Config-2.ps1
+++ aws-sharepoint/LCM-Config-2.ps1
@@ -1,12 +1,12 @@
 # This block sets the LCM configuration to what we need for QS
-[DSCLocalConfigurationManager()]
+#[DSCLocalConfigurationManager()]
 
-"Setting Execution Policy to Remote Signed"
+#"Setting Execution Policy to Remote Signed"
 Set-ExecutionPolicy RemoteSigned -Force
 
 # ~dk The command below fails, unless i do this first:
 # https://social.technet.microsoft.com/Forums/scriptcenter/en-US/d172080c-ea6e-4ea1-becb-5a41df8e743b/dsc-configuration-file-execution-error?forum=ITCG
-winrm quickconfig -quiet
+#winrm quickconfig -quiet
 
 # Sets LCM Configuration to MOF generated in previous command
 Set-DscLocalConfigurationManager -Path 'C:\AWSQuickstart\LCMConfig' -Force
