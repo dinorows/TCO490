c3e7ccaf3a7a9fdf8641fd7d8bc6c82f26d5dbac
dino konstantopoulos <dinorows@users.noreply.github.com>
2019-06-21 21:07:10 -0400

    Update LCM-Config-2.ps1
    to fix Set-DscLocalConfigurationManager call (added winrm quickconfig -quiet)

--- aws-sharepoint/LCM-Config-2.ps1
+++ aws-sharepoint/LCM-Config-2.ps1
@@ -4,5 +4,9 @@
 "Setting Execution Policy to Remote Signed"
 Set-ExecutionPolicy RemoteSigned -Force
 
+# ~dk The command below fails, unless i do this first:
+# https://social.technet.microsoft.com/Forums/scriptcenter/en-US/d172080c-ea6e-4ea1-becb-5a41df8e743b/dsc-configuration-file-execution-error?forum=ITCG
+winrm quickconfig -quiet
+
 # Sets LCM Configuration to MOF generated in previous command
 Set-DscLocalConfigurationManager -Path 'C:\AWSQuickstart\LCMConfig' -Force
